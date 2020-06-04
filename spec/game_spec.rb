890ce2dbd42490ba7e85f11b0fc3b68b3cd96e1f
Wyatt Pierce <piercewy@oregonstate.edu>
2020-06-04 08:25:20 -0700

    Adjust object initialization in game spec.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../lib/game'
 
 describe 'A game' do
+  game = Game.new
 
   it 'can start' do
-    Game.new.start
+    game.start
   end
 
   it 'is not initially started' do
-    game = Game.new
     expect(game).to_not be_started
   end
 
