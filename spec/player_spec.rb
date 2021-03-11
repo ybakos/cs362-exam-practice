4a03a8d0be623997d98e8ef1f8e21c70be25dd8b
Orion Junkins <junkinso@oregonstate.edu>
2021-03-11 11:03:04 -0800

    Player: spec refactored

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,15 +1,19 @@
 require_relative '../lib/player'
 
 describe 'A player' do
+  let(:defualt_player) {Player.new()}
+
+  let(:firstname) {"first"}
+  let(:lastname) {"last"}
+  let(:named_player) {Player.new(firstname, lastname)}
 
   it 'has a default name if no params are given'do
-    player = Player.new
-    expect(player.name).to eq("DefaultFirst DefaultLast")
+    expect(defualt_player.name).to eq("DefaultFirst DefaultLast")
   end
 
   it 'stores the first and last name as given in params' do
-    player = Player.new("first", "last")
-    expect(player.name).to eq("first last")
+    full_name = firstname + " " + lastname
+    expect(named_player.name).to eq(full_name)
   end
 
 end
