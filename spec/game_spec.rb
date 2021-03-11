442b73cd250ad2be186d008541c7061ef2b05543
Orion Junkins <junkinso@oregonstate.edu>
2021-03-11 10:59:52 -0800

    Game: spec refactored slightly

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,23 +1,20 @@
 require_relative '../lib/game'
 
 describe 'A game' do
-
-  it 'exists' do
-    Game.new
-  end
-
-  it 'can start' do
-    Game.new.start
-  end
+  let(:player) {Player.new("Firstname", "Lastname")}
+  let(:game) {Game.new(player)}
 
   it 'is not initially started' do
-    game = Game.new
     expect(game).to_not be_started
   end
 
+  it 'can start' do
+    expect(game).to_not be_started
+    game.start
+    expect(game).to be_started
+  end
+
   it 'has a player as given in the constructor' do
-    player = Player.new()
-    game = Game.new(player)
     game_player = game.player
     expect(game_player).to eq(player)
   end
