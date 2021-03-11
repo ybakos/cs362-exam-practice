f399ddcdd39d0e8a8401587f9e188de8402c493e
Orion Junkins <junkinso@oregonstate.edu>
2021-03-11 10:48:58 -0800

    Player: default name updated

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,7 +4,7 @@ describe 'A player' do
 
   it 'has a default name if no params are given'do
     player = Player.new
-    expect(player.name).to eq("FakeFirstName FakeLastName")
+    expect(player.name).to eq("DefaultFirst DefaultLast")
   end
 
   it 'stores the first and last name as given in params' do
