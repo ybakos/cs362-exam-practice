d235d25c1c37fea46cc9198b2d69f60fdef8dc08
Orion Junkins <junkinso@oregonstate.edu>
2021-03-15 09:09:45 -0700

    Player: name fields added with tests. Spec refactored

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,8 @@
 class Player
-  def initialize
+  attr_reader :first_name, :last_name
+  
+  def initialize(first, last)
+    @first_name = first
+    @last_name = last
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,14 @@
 require_relative '../lib/player'
 
 describe 'A Player' do
-  it 'can be initialized' do
-    player = Player.new
+  let(:player) {Player.new("first", "last")}
+
+  it 'has a first name' do
+    expect(player.first_name).to eq("first")
   end
+
+  it 'has a last name' do
+    expect(player.last_name).to eq("last")
+  end
+  
 end
