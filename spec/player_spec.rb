39d38037f21ab57c866f04efce11f8f9521009a7
Orion Junkins <junkinso@oregonstate.edu>
2021-03-15 09:20:43 -0700

    Player: spec refactored

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,14 +1,16 @@
 require_relative '../lib/player'
 
 describe 'A Player' do
-  let(:player) {Player.new("first", "last")}
+  let(:first_name) {'first'}
+  let(:last_name) {'last'}
+  let(:player) {Player.new(first_name, last_name)}
 
   it 'has a first name' do
-    expect(player.first_name).to eq("first")
+    expect(player.first_name).to eq(first_name)
   end
 
   it 'has a last name' do
-    expect(player.last_name).to eq("last")
+    expect(player.last_name).to eq(last_name)
   end
 
   it 'has a default name if none is given' do
@@ -16,4 +18,5 @@ describe 'A Player' do
     expect(default_player.first_name).to eq("default")
     expect(default_player.last_name).to eq("default")
   end
+
 end
