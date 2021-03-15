81ce3f013122670b2f1b2b5c70a482a58a762df8
Orion Junkins <junkinso@oregonstate.edu>
2021-03-15 09:03:41 -0700

    game_spec: redundant tests removed; all refactored

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,22 +1,13 @@
 require_relative '../lib/game'
 
 describe 'A game' do
-
-  it 'exists' do
-    Game.new
-  end
-
-  it 'can start' do
-    Game.new.start
-  end
-
+  let(:game) {Game.new}
+  
   it 'is not initially started' do
-    game = Game.new
     expect(game).to_not be_started
   end
 
   it 'can be started' do
-    game = Game.new
     game.start
     expect(game).to be_started
   end
