7fb5d8e744cac78ebb37bf0a68d40e5f5722b0b3
Erin Szabo <eszabo@cocc.edu>
2022-03-10 11:54:58 -0800

    Setup tests for Player.

---
+++ lib/player.rb
@@ -0,0 +1,6 @@
+# The program should have a Player, and every player has a first name and last name. 
+# A Game should "have" a player, and the player object should also be accessible via a method in the Game class.
+
+class Player
+  #
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,12 @@ require_relative '../lib/player'
 
 describe 'a player' do
   it "exists" do
-    skip
+    Player.new
   end
+
+  it "has a first name" 
+  it "has a last name"
+  it "belongs to game" 
+  it "is accessible by game"
+
 end
