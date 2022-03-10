c9574b4c0764391400de55448e58583259973726
Erin Szabo <eszabo@cocc.edu>
2022-03-10 13:44:59 -0800

    Test that a player can exist and that it has a first and last name

--- lib/player.rb
+++ lib/player.rb
@@ -2,5 +2,11 @@
 # A Game should "have" a player, and the player object should also be accessible via a method in the Game class.
 
 class Player
-  #
+
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,11 +2,19 @@ require_relative '../lib/player'
 
 describe 'a player' do
   it "exists" do
-    Player.new
+    Player.new("fake", "name")
   end
 
-  it "has a first name" 
-  it "has a last name"
+  it "has a first name" do 
+    name = Player.new("fake", "name").first_name
+    expect(name).to eq("fake")
+  end
+
+  it "has a last name" do
+    name = Player.new("fake", "name").last_name
+    expect(name).to eq("name")
+  end
+
   it "belongs to game" 
   it "is accessible by game"
 
