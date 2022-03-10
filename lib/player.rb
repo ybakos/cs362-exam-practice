463a3b233dc5b8ccdbcba597b96d05f6e805f527
Erin Szabo <eszabo@cocc.edu>
2022-03-10 14:56:00 -0800

    Reformat.

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,3 @@
-# The program should have a Player, and every player has a first name and last name. 
-# A Game should "have" a player, and the player object should also be accessible via a method in the Game class.
-
 class Player
 
   attr_accessor :first_name, :last_name
@@ -9,5 +6,5 @@ class Player
     @first_name = first_name
     @last_name = last_name
   end
-  
+
 end
