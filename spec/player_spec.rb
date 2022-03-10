ccb6c2fc134d1bd80be188dcb09c3c50d318d680
joeyst <joeystenbeck@gmail.com>
2022-03-10 11:35:22 -0800

    player_spec: extract Player.new into let statement

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,28 +2,26 @@ require './lib/player'
 
 describe Player do
 
+  let (:player) { Player.new }
+
   it "exists" do
-    Player.new
+    player
   end
 
   it "can set a first name" do
-    player = Player.new 
     player.set_first_name('hai')
   end
 
   it "can get a first name" do
-    player = Player.new
     player.set_first_name('hai')
     expect(player.get_first_name).to eql('hai')
   end
 
   it "can set a last name" do
-    player = Player.new
     player.set_last_name('hai')
   end
 
   it "can get a last name" do
-    player = Player.new
     player.set_last_name('hai')
     expect(player.get_last_name).to eql('hai')
   end
