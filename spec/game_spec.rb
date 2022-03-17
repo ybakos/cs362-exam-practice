40892683b09348ca30af40bf7ec261222f6babe7
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-03-17 15:17:59 -0700

    did stuff

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,12 +7,10 @@ describe 'A game' do
   end
 
   it 'can start' do
-    skip
     Game.new.start
   end
 
   it 'is not initially started' do
-    skip
     game = Game.new
     expect(game).to_not be_started
   end
